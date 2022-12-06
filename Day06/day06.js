12ee6f42da972cb09d003129c14a35e83b7a434f
KearneyJoe <44685213+KearneyJoe@users.noreply.github.com>
2022-12-06 09:22:40 -0500

    day 6 advent of code

--- Day06/day06.js
+++ Day06/day06.js
@@ -1,5 +1,34 @@
 const fs = require("fs");
 
 const data = fs
-    .readFileSync("day05.txt", { encoding: "utf-8" })
-    .split("\n")
+    .readFileSync("day06.txt", { encoding: "utf-8" })
+
+// console.log(data)
+
+//Part 1;
+//identify the first position where the four most recently received characters were all different
+
+//Sliding window using diffChecker to create a Set and check it's size
+function subRoutine(data, num) {
+    let vals = [...data.slice(0, num).split('')]
+    for (let i = num; i < data.length; i++) {
+        if (diffChecker(vals, num)) {
+            return i;
+        } else {
+            vals.shift()
+            vals.push(data[i])
+        }
+    }
+}
+
+function diffChecker(arr, num) {
+    let setCheck = new Set(arr)
+    if (setCheck.size == num) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(subRoutine(data, 4)) //Part 1: 1155
+console.log(subRoutine(data, 14)) //Part 2: 2789

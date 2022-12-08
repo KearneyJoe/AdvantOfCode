c64963f794db61748458f444ec174c8d1d400a31
KearneyJoe <44685213+KearneyJoe@users.noreply.github.com>
2022-12-08 16:14:34 -0500

    Day 7 advent of code - solution does not work yet

--- Day07/firstSolution.js
+++ Day07/firstSolution.js
@@ -7,7 +7,7 @@ const data = fs
 function sizeChecker(data, pwd = 'home', sizeObj = {
     'home': {
         'parent': null,
-        'child': [],
+        'child': null,
         'value': 0
     }
 }) {
@@ -27,57 +27,35 @@ function sizeChecker(data, pwd = 'home', sizeObj = {
         } else if (data[0].slice(0, 4) == '$ cd') {
             let temp = data[0].split(' ')
             let dirName = temp[temp.length - 1]
-
-            if (sizeObj[pwd]['child'].indexOf(dirName) == -1) {
-                sizeObj[pwd]['child'].push(dirName)
-                sizeObj[dirName] = { 'parent': pwd, 'child': [], 'value': 0 }
-            }
+            sizeObj[pwd]['child'] = dirName
+            sizeObj[dirName] = { 'parent': pwd, 'child': null, 'value': 0 }
             pwd = dirName
         }
         sizeChecker(data.slice(1,), pwd, sizeObj)
     }
     return sizeObj
 }
-
-console.log("MAIN DATA: ", sizeChecker(data))
-
 let sizeObject = sizeChecker(data)
 let dirSizes = {}
 for (const [key, value] of Object.entries(sizeObject)) {
-    dirSizes[key] = 0
-    if (sizeObject[key]['child'].length == 0) {
-        dirSizes[key] = sizeObject[key]['value']
-    } else {
-        for (let i = 0; i < sizeObject[key]['child'].length; i++) {
-            dirSizes[key] = sizeObject[key]['value'] + sizeAdder(sizeObject[key]['child'][i], sizeObject)
-        }
+    if (key != 'home') {
+        dirSizes[key] = sizeAdder(key, sizeObject)
     }
 }
 
 function sizeAdder(key, sizeObject, size = 0) {
-    if (sizeObject[key]['child'].length == 0) {
-        return size + sizeObject[key]['value']
+    if (sizeObject[key]['child'] == null) {
+        return sizeObject[key]['value']
     } else {
         let size = sizeObject[key]['value']
-        let subtotal = 0
-        for (let i = 0; i < sizeObject[key]['child'].length; i++) {
-            subtotal += sizeAdder(sizeObject[key]['child'][i], sizeObject)
-        }
-        return size + subtotal
+        return size + sizeAdder(sizeObject[key]['child'], sizeObject, size)
     }
 }
-
-console.log(dirSizes)
-// console.log(sizeObject)
+console.log(sizeObject)
 let part1Total = 0
 for (const [key, value] of Object.entries(dirSizes)) {
-    if (value <= 100000) {
+    if (value < 100000) {
         part1Total += value
     }
 }
-console.log(part1Total)
-
-
-
-//Part 1: FIRST GUESS 1191739
-//Part 1: SECOND GUESS 1493377
+console.log(part1Total) //Part 1: FIRST GUESS 1191739
